90b730fd1dec6bc9abce4aa1681aadab9a5d5f7c
Kenta Fukuchi <kfukuchi0@gmail.com>
2024-01-13 01:05:36 -0600

    Initial Commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let y;
+
+    let tabs = [
+        {name: 'Projects', link: '#projects'},
+        {name: 'About me', link: '#about'}
+]
+</script>
+
+<header class="{'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid ' + (
+    y > 0 ? 'py-4 bg-slate-950 border-violet-950' : 'py-6 bg-transparent border-transparent'
+)}">
+    <h1 class="font-medium4">
+        <b>Kenta</b> Fukuchi
+        <div class="">
+            {#each tabs as tab, index}
+                <a href="">
+                    <p>{tab.name}</p>
+                </a>
+            {/each}
+        </div>
+    </h1>
+</header>
